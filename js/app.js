68e407e56ad9e1a1a9fee7e625ab6499381c6e3c
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-04-21 19:26:38 -0300

    include css

--- js/app.js
+++ js/app.js
@@ -6,5 +6,9 @@ angular
             templateUrl: "/home.html",
             controller: "Controller"
         })
+        .when("/teste", {
+            templateUrl: "/teste.html",
+            controller: "Controller"
+        })
         .otherwise({redirectTo: "/"})
 })
